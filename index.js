c0bca1ad5814408e29882105c95cd20db620a84d
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-27 18:06:14 +0600

    Single product get by id api added.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //MongoDB connection
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8eefy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,15 @@ async function run() {
         app.get("/allProducts", async (req, res) => {
             const products = await productCollection.find({}).toArray();
             res.send(products);
+        });
+
+        //Get a single product by id
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const product = await productCollection.findOne({
+                _id: new ObjectId(id),
+            });
+            res.send(product);
         });
 
         //Post a product
